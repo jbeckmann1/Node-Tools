51027bed77747c9fb139fdf16ec51befa9f0c12b
jbeckmann1 <jo.bec@t-online.de>
2020-12-26 11:10:39 +0100

    Implement different ways to see if it is a file or folder(Promise based)

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 #!/usr/bin/env node
 const fs = require('fs');
+// Method#2
+// const util = require('util');
+// const lstat = util.promisify(fs.lstat);
+// Method#3
+const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), (err, filenames) => {
+//Für alle
+fs.readdir(process.cwd(), async (err, filenames) => {
 	if (err) {
 		console.log(err);
 	}
-	console.log(filenames);
+	// 	for (let filename of filenames) {
+	// 		try {
+	// 			const stats = await lstat(filename);
+	// 			console.log(filename, stats.isFile());
+	// 		} catch (err) {
+	// 			console.log(err);
+	// 		}
+	// 	}
+	// });
+	// //Method#1
+	// const lstat = (filename) => {
+	// 	return new Promise((resolve, reject) => {
+	// 		fs.lstat(filename, (err, stat) => {
+	// 			if (err) {
+	// 				reject(err);
+	// 			}
+	// 			resolve(stat);
+	// 		});
+	// 	});
+	// };
+	//"Good Solution"
+	const statPromises = filenames.map((filename) => {
+		return lstat(filename);
+	});
+	const allStats = await Promise.all(statPromises);
+	for (let stats of allStats) {
+		const index = allStats.indexOf(stats);
+		console.log(filenames[index], stats.isFile());
+	}
 });
